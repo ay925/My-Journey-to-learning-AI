9100b4b01df21f711bb5bce6f4a6e7b7b6c02d4d
Anupam Yadav <anupamyadav4347@gmail.com>
2025-11-23 18:12:00 +0530

    add list

---
+++ 01_basics_python/list.py
@@ -0,0 +1,34 @@
+tea_varities=["Black","Green","Oolond","White"]
+# print(tea_varities)
+# print(tea_varities[-1])
+# print(tea_varities[1:3])
+# tea_varities[3]="herable"
+# print(tea_varities)
+# tea_varities[1:2]=["Lemon"]
+# print(tea_varities)
+# tea_varities[1:3]=["Green"]
+# print(tea_varities)
+# tea_varities[1:3]=["Green","Masala"]
+# print(tea_varities)
+# print(tea_varities[1:1])
+# tea_varities[1:1]=["test","test"]
+# print(tea_varities)
+# tea_varities[1:3]=[]
+# print(tea_varities)
+# for tea in tea_varities:
+# #     print(tea)
+#       print(tea,end="-")
+# tea_varities.append("oolong")
+# if "oolong" in tea_varities:
+#       print("I have Olong tea")
+# tea_varities.pop()
+# print(tea_varities)
+# tea_varities.remove("Green")
+# print(tea_varities)
+# tea_varities.insert(1,"green")
+# print(tea_varities)
+# tea_varities_copy=tea_varities.copy()
+# squred_nums=[x**2 for x in range(1,10)]
+# print(squred_nums)
+cube_num=[y**3 for y in range(1,10)]
+print(cube_num)
